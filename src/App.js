52a75241c20c4e13cabdb3a53ef225f8c72323e7
Darko <darko.bogovic92@gmail.com>
2019-01-04 00:44:47 +0100

    adding complete and delete functions

--- src/App.js
+++ src/App.js
@@ -3,10 +3,53 @@ import './App.css';
 import Todos from './components/Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Learning React',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: true
+      },
+    ]
+  }
+
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    });
+  }
+
+  // Delete Todo
+  deleteTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos />
+        <Todos 
+          todos={this.state.todos} 
+          markComplete={this.markComplete} 
+          deleteTodo={this.deleteTodo}
+        />
       </div>
     );
   }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: '#f0f0f0',
+      padding: '10px',
+      borderBottom: '1px #ccc solid',
+      textDecoration: this.props.todo.completed ?
+      'line-through' : 'none'
+    }
+  }
+
+  render() {
+    const { id, title } = this.props.todo;
+    return (
+      <div style={this.getStyle()}>
+        <p>
+        <input 
+          type='checkbox' 
+          onChange={this.props.markComplete.bind(this, id)} 
+        />
+        { title }
+        <button onClick={this.props.deleteTodo.bind(this, id)} style={btnStyle}>X</button>
+        </p>
+      </div>
+    )
+  }
+}
+
+// PropTypes
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+  color: '#FF305D',
+  cursor: 'pointer',
+  float: 'right'
+}
+
+export default TodoItem;
